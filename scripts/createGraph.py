851f447ad6a8e1b6c2cf47f032818438eca6405a
Rolf <>
2020-08-02 13:43:16 +0200

    Update createGraph.py
    prepare for pedictions

--- scripts/createGraph.py
+++ scripts/createGraph.py
@@ -1,17 +1,39 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 #
 # pip3 install matplotlib
 
 from matplotlib import pyplot as plt
 from dateutil import parser
+from statistics import mean
 import datetime
-import json 
+import json
+
+
+def getDateRange(metenisweten):
+    for datum in metenisweten:
+        try:
+            mindatum
+        except NameError:
+            mindatum = parser.parse(datum)
+
+        try:
+            maxdatum
+        except NameError:
+            maxdatum = parser.parse(datum)
+
+        mindatum = min(mindatum, parser.parse(datum))
+        maxdatum = max(maxdatum, parser.parse(datum))
+
+    date_range = [mindatum + datetime.timedelta(days=x)
+                  for x in range(0, (maxdatum-mindatum).days+30)]
+    return date_range
+
 
 print("Generating graphs.")
 
 with open('../cache/COVID-19_casus_landelijk.json', 'r') as json_file:
     data = json.load(json_file)
-    metenisweten = { }
+    metenisweten = {}
     for record in data:
         if (record['Date_statistics'] not in metenisweten):
             metenisweten[record['Date_statistics']] = {
@@ -31,16 +53,22 @@ with open('../cache/NICE-intake-count.json', 'r') as json_file:
             }
         metenisweten[measurement['date']]['nu_op_ic'] += measurement['value']
 
-x = []
-y = []
+positief = {
+    'x': [],
+    'y': []
+}
 
-a = []
-avg = 0
-avgsize = 14
+positief_gemiddeld = {
+    'x': [],
+    'y': [],
+    'avgsize': 14
+}
 
-z = []
-ziek = 0
-ziekteduur = 14
+ziek = {
+    'x' : [],
+    'y' : [],
+    'ziekteduur' : 14
+}
 
 i = []
 
@@ -48,76 +76,82 @@ i = []
 # print((parser.parse("2020-01-30") - datetime.timedelta(days=ziekteduur)).strftime("%Y-%m-%d"))
 # exit
 
-for datum in metenisweten:
-    try:
-        mindatum
-    except NameError:
-        mindatum = parser.parse(datum)
-
-    try:
-        maxdatum
-    except NameError:
-        maxdatum = parser.parse(datum)
-
-    mindatum = min(mindatum, parser.parse(datum))
-    maxdatum = max(maxdatum, parser.parse(datum))
-
-date_range = [mindatum + datetime.timedelta(days=x) for x in range(0, (maxdatum-mindatum).days+30)]    
+date_range = getDateRange(metenisweten)
 
 for d in date_range:
     datum = d.strftime("%Y-%m-%d")
+
     if datum not in metenisweten:
         continue
 
-    x.append(parser.parse(datum))
-    y.append(metenisweten[datum]['positief'])
+    positief['x'].append(parser.parse(datum))
+    positief['y'].append(metenisweten[datum]['positief'])
     i.append(metenisweten[datum]['nu_op_ic']*10)
-   
-    avg = (avg * (avgsize-1) /avgsize) + (metenisweten[datum]['positief'] / avgsize)
-    a.append(avg)
 
-    beterdag = (parser.parse(datum) - datetime.timedelta(days=ziekteduur)).strftime("%Y-%m-%d")
-    ziek = ziek + metenisweten[datum]['positief']
+    avg = mean(positief['y'][len(positief['y'])-11:])
+    positief_gemiddeld['x'].append(parser.parse(
+        datum) - datetime.timedelta(days=positief_gemiddeld['avgsize']/2))
+    positief_gemiddeld['y'].append(avg)
+
+    beterdag = (parser.parse(datum) -
+                datetime.timedelta(days=ziek['ziekteduur'])).strftime("%Y-%m-%d")
+
+    try:
+        nuziek = nuziek + metenisweten[datum]['positief']
+    except NameError:
+        nuziek = metenisweten[datum]['positief']
+
     if beterdag in metenisweten:
-        ziek = ziek - metenisweten[beterdag]['positief']
-    z.append(ziek)
+        nuziek = nuziek - metenisweten[beterdag]['positief']
 
+    ziek['x'].append(parser.parse(datum))
+    ziek['y'].append(nuziek)
 
 
 def anotate(plt, metenisweten, datum, tekst, x, y):
     plt.annotate(
-        tekst, 
+        tekst,
         xy=(parser.parse(datum), metenisweten[datum]['positief']),
         xytext=(parser.parse(x), y),
         bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
-        arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0')
+        arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0')
     )
 
-fig, ax1 = plt.subplots(figsize=(10,5))
+
+fig, ax1 = plt.subplots(figsize=(10, 5))
 ax2 = plt.twinx()
 
 #plt.figure(figsize =(10,5))
-ax1.grid(which='both', axis='both', linestyle='-.', color='gray', linewidth=1, alpha=0.3)
-ax2.grid(which='both', axis='both', linestyle='-.', color='gray', linewidth=1, alpha=0.3)
+ax1.grid(which='both', axis='both', linestyle='-.',
+         color='gray', linewidth=1, alpha=0.3)
+ax2.grid(which='both', axis='both', linestyle='-.',
+         color='gray', linewidth=1, alpha=0.3)
 
 # Plot cases per dag
-ax1.plot(x,y,label='positief getest')
+ax1.plot(positief['x'], positief['y'], label='positief getest')
 
-anotate(ax1, metenisweten, "2020-03-09", 'Brabant geen\nhanden schudden', "2020-01-20", 300)
-anotate(ax1, metenisweten, "2020-03-15", 'Onderwijs\nverpleeghuis\nhoreca\ndicht', "2020-02-01", 600)
-anotate(ax1, metenisweten, "2020-03-23", '1,5 meter, €400 boete', "2020-01-15", 1000)
+anotate(ax1, metenisweten, "2020-03-09",
+        'Brabant geen\nhanden schudden', "2020-01-20", 300)
+anotate(ax1, metenisweten, "2020-03-15",
+        'Onderwijs\nverpleeghuis\nhoreca\ndicht', "2020-02-01", 600)
+anotate(ax1, metenisweten, "2020-03-23",
+        '1,5 meter, €400 boete', "2020-01-15", 1000)
 anotate(ax1, metenisweten, "2020-04-22", 'Scholen 50% open', "2020-04-25", 900)
-anotate(ax1, metenisweten, "2020-05-11", 'Scholen, kapers,\ntandarts open', "2020-05-08", 500)
+anotate(ax1, metenisweten, "2020-05-11",
+        'Scholen, kapers,\ntandarts open', "2020-05-08", 500)
 anotate(ax1, metenisweten, "2020-06-01", 'Terassen open', "2020-05-25", 310)
-anotate(ax1, metenisweten, "2020-07-01", 'Maatregelen afgezwakt\nAlleen nog 1,5 meter\nmondkapje in OV', "2020-06-20", 550)
+anotate(ax1, metenisweten, "2020-07-01",
+        'Maatregelen afgezwakt\nAlleen nog 1,5 meter\nmondkapje in OV', "2020-06-20", 550)
 
 # Plot average per dag
-ax = a[int(avgsize/2):]
-xx = x[:len(ax)]
-ax1.plot(xx,ax,color='cyan', linestyle=':', label=str(avgsize)+' daags gemiddelde, t-'+str(int(avgsize/2)))
+ax1.plot(positief_gemiddeld['x'], positief_gemiddeld['y'], color='cyan', linestyle=':',
+         label=str(positief_gemiddeld['avgsize'])+' daags gemiddelde, t-' +
+         str(int(positief_gemiddeld['avgsize']/2))
+         )
 
-ax2.plot(x,z,color='orange', linestyle=':', label='aantal getest ziek')
-ax2.plot(x,i,color='red', label='aantal nu op IC (*10)')
+ax2.plot(ziek['x'], ziek['y'], color='orange',
+         linestyle=':', label='aantal getest ziek')
+ax2.plot(positief['x'], i, color='red', label='aantal nu op IC (*10)')
 
 ax1.set_xlabel("Datum")
 ax1.set_ylabel("Positief getest per dag")
@@ -134,4 +168,4 @@ plt.title('COVID-19 besmettingen, '+filedate)
 ax1.legend(loc="upper left")
 ax2.legend(loc="upper right")
 plt.savefig("../graphs/besmettingen.png")
-#plt.show()
+# plt.show()
