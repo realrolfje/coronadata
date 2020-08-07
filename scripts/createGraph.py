0a21e39409f7d1118061fc595c8a7730bbfe9532
Rolf <>
2020-08-07 13:45:50 +0200

    Ran getDailyExcelData

--- scripts/createGraph.py
+++ scripts/createGraph.py
@@ -270,10 +270,10 @@ gegenereerd_op=datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 plt.title('COVID-19 besmettingen, '+gegenereerd_op)
 
 footerleft="Gegenereerd op "+gegenereerd_op+". Publicatiedatum RIVM "+filedate+"."
-plt.figtext(0.01, 0.01, footerleft, ha="left", fontsize=8)
+plt.figtext(0.01, 0.01, footerleft, ha="left", fontsize=8, color="gray")
 
 footerright="Bronnen: https://data.rivm.nl/covid-19, https://www.stichting-nice.nl/covid-19/"
-plt.figtext(0.99, 0.01, footerright, ha="right", fontsize=8)
+plt.figtext(0.99, 0.01, footerright, ha="right", fontsize=8, color="gray")
 
 
 ax1.legend(loc="upper left")
