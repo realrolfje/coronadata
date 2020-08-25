5e98e51d05ab44e1f8acc61343a7756e442b5f43
Rolf <>
2020-08-25 21:20:32 +0200

    Ran getDailyExcelData

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ cd scripts
 ./calculateDailyExcelData.py
 ./createGraph.py
 cd ..
-echo "< updated $(date) -->" >> docs/index.html
+echo "<!-- updated $(date) -->" >> docs/index.html
 git add .
 git commit -m 'Ran getDailyExcelData'
 git push
