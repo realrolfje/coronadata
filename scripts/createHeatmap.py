443976f97db369411c00439cd746ea84daabac96
Rolf <>
2020-09-10 21:01:50 +0200

    Ran getDailyExcelData

--- scripts/createHeatmap.py
+++ scripts/createHeatmap.py
@@ -49,25 +49,6 @@ weights=[]
 for d in x:
     weights.append(1./weightsmap[d])
 
-
-def decimalstring(number):
-    return "{:,}".format(number).replace(',', '.')
-
-gegenereerd_op=datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-
-
-fig, heatmap = plt.subplots(figsize=(10, 5))
-fig.subplots_adjust(bottom=0.2, left=0.09, right=0.91)
-averages = plt.twinx()
-
-plt.title('Besmettingen per leeftijdsgroep, '+gegenereerd_op)
-
-# Gewogen:
-#plt.hist2d(x, y, bins=[x[-1]+7,10], range=[[0,x[-1]+7],[0,100]], cmap='inferno', weights=weights)
-
-# Ongewogen:
-heatmap.hist2d(x, y, bins=[x[-1]+7,10], range=[[0,x[-1]+7],[0,100]], cmin=1, cmap='Blues') # inferno is also a good one
-
 # Averages
 gemiddeldeleeftijd = {
     'x': [],
@@ -87,8 +68,32 @@ for d in date_range:
             som += metenisweten[datum]['besmettingleeftijd'][age] * int(age)
             positief += metenisweten[datum]['besmettingleeftijd'][age]
         gemiddeld = som/positief
-        gemiddeldeleeftijd['y'] = gemiddeld
+        gemiddeldeleeftijd['y'].append(gemiddeld)
 
+def decimalstring(number):
+    return "{:,}".format(number).replace(',', '.')
+
+gegenereerd_op=datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+
+fig, heatmap = plt.subplots(figsize=(10, 5))
+fig.subplots_adjust(bottom=0.2, left=0.09, right=0.91)
+averages = plt.twinx()
+
+plt.title('Besmettingen per leeftijdsgroep, '+gegenereerd_op)
+
+
+# Gewogen:
+#plt.hist2d(x, y, bins=[x[-1]+7,10], range=[[0,x[-1]+7],[0,100]], cmap='inferno', weights=weights)
+
+# Ongewogen:
+heatmap.hist2d(x, y, bins=[x[-1]+7,10], range=[[0,x[-1]+7],[0,100]], cmin=1, cmap='Blues') # inferno is also a good one
+
+averages.plot(gemiddeldeleeftijd['x'], gemiddeldeleeftijd['y'], color='darkred', alpha=0.5, label='Gemiddelde leeftijd (nu: '+str(int(gemiddeldeleeftijd['y'][-1]))+')')
+
+averages.legend(loc="upper right")
+
+heatmap.set_ylim([0, 100])
+averages.set_ylim([0,100])
 
 # Dirty stuff to get x labels (needs cleanup)
 xlabeldates = [startdate + relativedelta(months=x) for x in range(xlabels[-1].month - startdate.month + 1)]
