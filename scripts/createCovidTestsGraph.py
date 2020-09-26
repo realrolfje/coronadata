1671ed7746e728410ef54ebf32f84fb775356929
Rolf <>
2020-09-27 00:30:21 +0200

    total tests

--- scripts/createCovidTestsGraph.py
+++ scripts/createCovidTestsGraph.py
@@ -20,6 +20,12 @@ positief = {
     'y': []
 }
 
+positief_voorspeld = {
+    'x': [],
+    'y': [],
+    'avgsize':12
+}
+
 totaaltests = {
     'x': [],
     'y': []
@@ -30,52 +36,6 @@ positief_percentage = {
     'y': []
 }
 
-
-opgenomen = {
-    'x': [],
-    'y': []
-}
-
-positief_gemiddeld = {
-    'x': [],
-    'y': [],
-    'avgsize': 14
-}
-
-positief_voorspeld = {
-    'x': [],
-    'y': [],
-    'avgsize': 12
-}
-
-geschat_ziek = {
-    'x'   : [],
-    'y'   : [],
-    'min' : [],
-    'max' : []
-}
-
-ic = {
-    'x' : [],
-    'y' : [],
-    'rc' : []
-}
-
-ic_voorspeld = {
-    'x' : [],
-    'y' : [],
-    'avgsize': 3
-}
-
-besmettingsgraad = {
-    'x' : [],
-    'y' : []    
-}
-
-# print("2020-01-30")
-# print((parser.parse("2020-01-30") - datetime.timedelta(days=ziekteduur)).strftime("%Y-%m-%d"))
-# exit
-
 date_range = brondata.getDateRange(metenisweten)
 
 for d in date_range:
@@ -85,12 +45,6 @@ for d in date_range:
     if datum in metenisweten and parser.parse(datum).date() <= datetime.date.today():
         positief['x'].append(parser.parse(datum))
         positief['y'].append(metenisweten[datum]['positief'])
-
-        ic['x'].append(parser.parse(datum))
-        ic['y'].append(metenisweten[datum]['nu_op_ic'])
-
-        opgenomen['x'].append(parser.parse(datum))
-        opgenomen['y'].append(metenisweten[datum]['opgenomen'])
 
         if metenisweten[datum]['rivm_totaal_tests']:
             totaaltests['x'].append(parser.parse(datum))
@@ -104,19 +58,6 @@ for d in date_range:
 
         if metenisweten[datum]['rivm-datum']:
             filedate = metenisweten[datum]['rivm-datum']
-
-        if len(ic['y'])>1:
-            ic['rc'].append(ic['y'][-1] - ic['y'][-2])
-        else:
-            ic['rc'].append(0)
-
-    # --------------------------------- Gemiddeld positief getest
-    if datum in metenisweten:
-        avg = mean(positief['y'][len(positief['y'])-11:])
-    else:
-        avg = mean(positief_gemiddeld['y'][len(positief_gemiddeld['y'])-11:])
-    positief_gemiddeld['x'].append(parser.parse(datum) - datetime.timedelta(days=positief_gemiddeld['avgsize']/2))
-    positief_gemiddeld['y'].append(avg)
 
     # ---------------------- Voorspelling positief getst obv gemiddelde richtingscoefficient positief getest.
     if datum in metenisweten and len(positief['y']) > positief_voorspeld['avgsize'] and parser.parse(datum) < (datetime.datetime.now() - datetime.timedelta(days=positief_voorspeld['avgsize'])):
@@ -134,33 +75,8 @@ for d in date_range:
         positief_voorspeld['x'].append(parser.parse(datum) + datetime.timedelta(days=1))
         positief_voorspeld['y'].append(positief['y'][-1])
 
-
-    # ---------------------- Voorspelling op IC obv gemiddelde richtingscoefficient positief getest.
-    if len(ic['x']) > 10 and parser.parse(datum) > ic['x'][-1]:
-        ic_rc = mean(ic['rc'][-5:])
-
-        ic_voorspeld['x'].append(parser.parse(datum))
-        ic_voorspeld['y'].append(ic['y'][-1] + ic_rc * (parser.parse(datum) - ic['x'][-1]).days )
-
-    # ----------------------- Trek "geschat ziek" op basis van RC nog even door.
-    deltadagen = 15
-    if datum in metenisweten and metenisweten[datum]['rivm_schatting_besmettelijk']['value']:
-        geschat_ziek['x'].append(parser.parse(datum))
-        geschat_ziek['y'].append(metenisweten[datum]['rivm_schatting_besmettelijk']['value'])
-        geschat_ziek['min'].append(metenisweten[datum]['rivm_schatting_besmettelijk']['min'])
-        geschat_ziek['max'].append(metenisweten[datum]['rivm_schatting_besmettelijk']['max'])
-        geschat_ziek_nu = metenisweten[datum]['rivm_schatting_besmettelijk']['value']
-    elif len(geschat_ziek['y']) > deltadagen:
-        vorig_datum = parser.parse(datum) - datetime.timedelta(days=deltadagen)
-        vorig_y = geschat_ziek['y'][-deltadagen]
-        nieuw_y = geschat_ziek['y'][-1] + (geschat_ziek['y'][-1] - vorig_y)/deltadagen
-        geschat_ziek['x'].append(parser.parse(datum))
-        geschat_ziek['y'].append(nieuw_y)
-
-
 def decimalstring(number):
     return "{:,}".format(number).replace(',','.')
-
 
 def anotate(plt, metenisweten, datum, tekst, x, y):
     if datum in metenisweten:
@@ -189,7 +105,7 @@ ax2.grid(which='both', axis='both', linestyle='-.',
 ax1.plot(positief['x'][:-10], positief['y'][:-10], color='steelblue', label='positief getest (totaal '+decimalstring(totaal_positief)+")")
 ax1.plot(positief['x'][-11:], positief['y'][-11:], color='steelblue', linestyle='--', alpha=0.3, label='onvolledig')
 
-ax1.text(parser.parse("2020-05-01"), 20000, "Geen smoesjes, je weet het best.\nAls je niet ziek wordt, hoef je ook niet getest.", color="gray")
+ax1.text(parser.parse("2020-05-01"), 21000, "Geen smoesjes, je weet het best.\nAls je niet ziek wordt, hoef je ook niet getest.", color="gray")
 
 ax1.plot(positief_voorspeld['x'][-17:], positief_voorspeld['y'][-17:], 
          color='steelblue', linestyle=':', label='voorspeld')
@@ -212,7 +128,7 @@ ax2.set_ylabel("Percentage positief getest")
 ax1.set_ylim([0, 40000])
 ax2.set_ylim([0, 40])
 
-plt.gca().set_xlim([parser.parse("2020-02-01"), ic_voorspeld['x'][-1]])
+plt.gca().set_xlim([parser.parse("2020-02-01"), positief_voorspeld['x'][-1]])
 
 gegenereerd_op=datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 
