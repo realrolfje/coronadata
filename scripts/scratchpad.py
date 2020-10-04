8c3e346f01c8595ac65b4cb74d08f2113eb7ace1
Rolf <>
2020-10-04 17:39:27 +0200

    scratchpad

--- scripts/scratchpad.py
+++ scripts/scratchpad.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 #
+from string import Template
 import re
 print(re.search('-\d{4}$','01-02-0003'))
+
+# Simple templating
+d = dict(who='rolf')
+print(Template('Give $who $$100').substitute(d))
+
+
+# This does not work
+e = {
+    'who' : 'rolf',
+    'what' : {
+        'currency' : 'eur'
+    }
+}
+print(Template('Give $who $$100 in ${what.currency}').substitute(e))
