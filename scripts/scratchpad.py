9846d02a06a81b07e8adbf2e6f6661415b0a4115
Rolf <>
2020-10-14 20:37:40 +0200

    typos

--- scripts/scratchpad.py
+++ scripts/scratchpad.py
@@ -5,18 +5,9 @@ from os import listdir
 from os.path import isfile, join, basename
 from modules.brondata import decimalstring
 
-
 d = dict(who='rolf')
 
 print(listdir())
-
-# Turns US 10,000.00 into EU 10.000,00
-def decimalstring(number):
-    if number == round(number) and len(str(number)) <= 4:
-        return str(number)
-    else:
-        return "{:,}".format(number).replace(',','x').replace('.',',').replace('x','.')
-
 
 print(decimalstring(1000))
 print(decimalstring(10000))
