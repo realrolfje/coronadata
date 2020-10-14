40f09b5af40e563449c46bf51dec9a0c459800f1
Rolf <>
2020-10-14 20:23:46 +0200

    add schattingen afwijking

--- update.sh
+++ update.sh
@@ -1,14 +1,5 @@
 git pull
-mkdir -p cache
-cd scripts
-./createHeatmap.py
-./createRtGraph.py
-./createRNAGraph.py
-./calculateDailyExcelData.py
-./createCovidTestsGraph.py
-./createZiekGraph.py
-./processTemplates.py
-cd ..
+./creategraphs.sh
 echo "<!-- updated $(date) -->" >> docs/index.html
 git add .
 git commit -m 'Ran update script'
