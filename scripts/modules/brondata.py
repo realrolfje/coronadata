69903bd73368c0e849f8c036985f0aeedb479bb2
Rolf <>
2021-05-08 13:37:50 +0200

    Ran update script

--- scripts/modules/brondata.py
+++ scripts/modules/brondata.py
@@ -92,7 +92,7 @@ def downloadMostRecentAppleMobilityReport(filename):
         print("Downloading fresh data to "+filename, end="...")
         for i in range(14):
             theday  = (datetime.date.today() - datetime.timedelta(days = i)).strftime("%Y-%m-%d")
-            url = 'https://covid19-static.cdn-apple.com/covid19-mobility-data/2106HotfixDev21/v3/en-us/applemobilitytrends-'+theday+'.csv'
+            url = 'https://covid19-static.cdn-apple.com/covid19-mobility-data/2107HotfixDev14/v3/en-us/applemobilitytrends-'+theday+'.csv'
             
             try:
                 urllib.request.urlretrieve(url, filename)
