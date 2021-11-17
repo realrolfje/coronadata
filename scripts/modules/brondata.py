26fcd7cb161e91c8653f33ad9ffad09b19401afc
Rolf <>
2021-11-17 23:55:58 +0100

    new file detection externalized

--- scripts/modules/brondata.py
+++ scripts/modules/brondata.py
@@ -94,7 +94,7 @@ def downloadMostRecentAppleMobilityReport(filename):
         return False
     else:
         print("Downloading fresh data to "+filename, end="...")
-        url = 'https://covid19-static.cdn-apple.com/covid19-mobility-data/2202HotfixDev8/v3/en-us/applemobilitytrends-2021-11-11.csv'
+        url = 'https://covid19-static.cdn-apple.com/covid19-mobility-data/2202HotfixDev13/v3/en-us/applemobilitytrends-2021-11-16.csv'
         try:
             urllib.request.urlretrieve(url, filename)
             print("done")
