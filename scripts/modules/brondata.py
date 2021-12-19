9fc76b522d6f087790eb9d8bd7c288c047291ca7
Rolf <>
2021-12-19 11:03:04 +0100

    persco toegevoegd

--- scripts/modules/brondata.py
+++ scripts/modules/brondata.py
@@ -94,7 +94,7 @@ def downloadMostRecentAppleMobilityReport(filename):
         return False
     else:
         print("Downloading fresh data to "+filename, end="...")
-        url = 'https://covid19-static.cdn-apple.com/covid19-mobility-data/2203HotfixDev20/v3/en-us/applemobilitytrends-2021-12-12.csv'
+        url = 'https://covid19-static.cdn-apple.com/covid19-mobility-data/2204HotfixDev11/v3/en-us/applemobilitytrends-2021-12-17.csv'
         try:
             urllib.request.urlretrieve(url, filename)
             print("done")
