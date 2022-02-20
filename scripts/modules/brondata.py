de749d52834d7b9eaec3d20fe46a209bc997ac60
Rolf <>
2022-02-20 12:49:01 +0100

    mobility update

--- scripts/modules/brondata.py
+++ scripts/modules/brondata.py
@@ -177,7 +177,7 @@ def downloadMostRecentAppleMobilityReport(filename):
         return False
     else:
         print("Downloading fresh data to "+filename, end="...")
-        url = 'https://covid19-static.cdn-apple.com/covid19-mobility-data/2207HotfixDev32/v3/en-us/applemobilitytrends-2022-02-10.csv'
+        url = 'https://covid19-static.cdn-apple.com/covid19-mobility-data/2208HotfixDev11/v3/en-us/applemobilitytrends-2022-02-18.csv'
         try:
             urllib.request.urlretrieve(url, filename)
             print("done")
