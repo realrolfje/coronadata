3a0062a896278844d24a0a0e09e808b6ce718e89
Rolf <>
2022-03-27 16:31:55 +0200

    nieuwe regels, verkeerdsdata

--- scripts/modules/brondata.py
+++ scripts/modules/brondata.py
@@ -177,7 +177,7 @@ def downloadMostRecentAppleMobilityReport(filename):
         return False
     else:
         print("Downloading fresh data to "+filename, end="...")
-        url = 'https://covid19-static.cdn-apple.com/covid19-mobility-data/2209HotfixDev15/v3/en-us/applemobilitytrends-2022-03-11.csv'
+        url = 'https://covid19-static.cdn-apple.com/covid19-mobility-data/2210HotfixDev16/v3/en-us/applemobilitytrends-2022-03-26.csv'
         try:
             urllib.request.urlretrieve(url, filename)
             print("done")
